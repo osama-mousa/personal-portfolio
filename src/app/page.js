dd466b90a60dcf79db208cc469df920b42d9d3f9
osamasayd <osamasayd141@gmail.com>
2024-02-04 21:43:20 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12">
+      <HeroSection />
+    </main>
+  );
+}
