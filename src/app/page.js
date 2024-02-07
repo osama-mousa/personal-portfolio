f4e02d0451f36c0c93005e5042a92b4739a6b04f
osamasayd <osamasayd141@gmail.com>
2024-02-07 22:59:02 +0300

    set AboutSection to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto py-4 px-12 mt-24">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
