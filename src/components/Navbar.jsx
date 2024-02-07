1fe7bcccd460bc8a714dd10f67361e8fad9eeff2
osamasayd <osamasayd141@gmail.com>
2024-02-07 12:45:08 +0300

    set MenuOverlay and update some css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import NavLink from './NavLink'
 import { Bars3Icon, XMarkIcon, XmarkIcon } from '@heroicons/react/24/solid'
+import MenuOverlay from './MenuOverlay'
 
 
 const navLinks = [
@@ -23,7 +24,7 @@ const navLinks = [
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false)
     return (
-        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
             <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
                 <div className="mobile-menu block md:hidden">
@@ -32,7 +33,7 @@ const Navbar = () => {
                             <Bars3Icon className='h-5 w-5' />
                         </button>
                     ) : (
-                        <button onClick={() => setNavbarOpen(false)}  className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                        <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
                             <XMarkIcon className='h-5 w-5' />
                         </button>
                     )}
@@ -47,6 +48,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
+            {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
         </nav>
     )
 }
