90f072e1e8119dac74993eea4d3431156b76d819
osamasayd <osamasayd141@gmail.com>
2024-02-07 12:28:42 +0300

    update page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mx-auto py-4 px-12 mt-24">
+        <HeroSection />
+      </div>
     </main>
   );
 }
