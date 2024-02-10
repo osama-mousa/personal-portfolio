648c754f3a83e2c4db72653f6a43cbb25f9b796f
osamasayd <osamasayd141@gmail.com>
2024-02-10 19:43:01 +0300

    adding TAB_DATA list

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,6 +3,42 @@ import Image from 'next/image'
 import React, { useState, useTransition } from 'react'
 import TabButton from './TabButton';
 
+const TAB_DATA = [
+    {
+        title: 'Skills',
+        id: 'skills',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>JavaScript</li>
+                <li>Tailwindcss</li>
+                <li>Cypress</li>
+                <li>Postman</li>
+                <li>Automated Testing</li>
+            </ul>
+        )
+    },
+    {
+        title: 'Education',
+        id: 'education',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Faculty of Computer Programming, Bartin University</li>
+            </ul>
+        )
+    },
+    {
+        title: 'Certifications',
+        id: 'certifications',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Certificate of Completion Cypress io</li>
+            </ul>
+        )
+    },
+]
+
 const AboutSection = () => {
     const [tab, setTab] = useState('skills');
     const [isPending, startTransition] = useTransition();
@@ -17,7 +53,7 @@ const AboutSection = () => {
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 sm:px-16'>
                 <Image src='/images/hand-hold-artificial-intelligence-phone-made-with-generative-al_812426-737.jpg' width={500} height={500} />
 
-                <div>
+                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl text-white font-bold mb-4'>About Me</h2>
                     <p className='text-base lg:text-lg'>I am a Software developer with a passion for creating
                         interactive and responsive web applications. I have experience
@@ -25,12 +61,13 @@ const AboutSection = () => {
                         looking to expand my knowledge and skill set. I am a team player and
                         I am excited to work with others to create amazing applications.
                     </p>
-                    <div className='flex flex-row mt-8 gap-3'>
+                    <div className='flex flex-row justify-start mt-8 gap-3'>
                         <TabButton selectTab={() => handleTabChange('skills')} active={tab === "skills"}>Skills</TabButton>
                         <TabButton selectTab={() => handleTabChange('education')} active={tab === "education"}>Education</TabButton>
                         <TabButton selectTab={() => handleTabChange('certifications')} active={tab === "certifications"}>Certifications</TabButton>
 
                     </div>
+                    <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
                 </div>
             </div>
         </section>
