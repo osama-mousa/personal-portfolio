24c17e7f04308113cdd59fdd765b522cc216ff9c
osamasayd <osamasayd141@gmail.com>
2024-03-03 23:50:19 +0300

    update hero and set onClick to buttons

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -26,16 +26,16 @@ const HeroSection = () => {
                         />
                     </h1>
                     <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ad facere sapiente voluptatibus nulla quaerat ut earum fugiat numquam.
+                        I&apos;m a passionate web developer skilled in JavaScript, React, Next.js, Cypress, and Git. Quick learner, team player, eager to collaborate on creating awesome applications.
                     </p>
                     <div className=''>
-                        <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800 via-purple-700 to-pink-800 hover:text-gray-300 hover:border hover:border-slate-800 text-[#121212] mr-4'>Hire Me</button>
-                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800 via-purple-700 to-pink-800 hover:bg-slate-800 text-white mt-4'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
+                        <button onClick={() => window.location.href = 'https://www.linkedin.com/in/osama-mousa-568364209'} className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800 via-purple-700 to-pink-800 hover:text-gray-300 hover:border hover:border-slate-800 text-[#121212] mr-4'>Hire Me</button>
+
+                        <button onClick={() => window.location.href = 'https://flowcv.com/resume/u93s6l54kg'} className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800 via-purple-700 to-pink-800 hover:bg-slate-800 text-white mt-4'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-                    <div className='rounded-full lg:rounded-none bg-purple-950  w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative glow '>
+                    <div className='rounded-full bg-purple-950  w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative glow'>
                         <Image
                             src={'/images/me.jpeg'}
                             alt='Hero Image'
