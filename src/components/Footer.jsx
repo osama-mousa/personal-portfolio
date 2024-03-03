b68a59ddc76dc3a05f7c713667e304a5e5eea345
osamasayd <osamasayd141@gmail.com>
2024-03-03 16:01:49 +0300

    create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='footer bg-[#111111] glow'>
+            <div className='container p-16 flex justify-between'>
+                <span>LOGO</span>
+                <p className='text-slate-600'>All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
