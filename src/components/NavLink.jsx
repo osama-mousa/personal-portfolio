62d52c795cad8c3a925df412276db646ebf82031
osamasayd <osamasayd141@gmail.com>
2024-03-05 01:39:43 +0300

    add scroll func

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link"
 
+const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+};
+
 const NavLink = ({ href, title }) => {
     return (
         <Link
-            href={href}
+            href={`#${href}`}
+            onClick={() => scrollToSection({href})}
             className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl font-semibold rounded md:p-0 hover:text-white'>
             {title}
         </Link>
