cbdb002c643fd073d407c7edcab617d561439c98
osamasayd <osamasayd141@gmail.com>
2024-03-05 00:08:42 +0300

    edit hero title and buttons color

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,8 +9,8 @@ const HeroSection = () => {
         <section>
             <div className='grid grid-cols-1 sm:grid-cols-12'>
                 <div className='col-span-7 place-self-center text-center sm:text-left'>
-                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-600'>Hello, I&apos;m{' '}</span>
+                    <h1 className="text-white mb-20 text-4xl sm:text-5xl lg:text-6xl font-extrabold h-10">
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-900 to-pink-900'>Hello, I&apos;m{' '}</span>
                         <TypeAnimation
                             sequence={[
                                 'Osama',
@@ -29,9 +29,9 @@ const HeroSection = () => {
                         I&apos;m a passionate web developer skilled in JavaScript, React, Next.js, Cypress, and Git. Quick learner, team player, eager to collaborate on creating awesome applications.
                     </p>
                     <div className=''>
-                        <button onClick={() => window.location.href = 'https://www.linkedin.com/in/osama-mousa-568364209'} className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800 via-purple-700 to-pink-800 hover:text-gray-300 hover:border hover:border-slate-800 text-[#121212] mr-4'>Hire Me</button>
+                        <button onClick={() => window.location.href = 'https://www.linkedin.com/in/osama-mousa-568364209'} className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-950  hover:text-white hover:border hover:border-slate-900 text-gray-300 mr-5'>Hire Me</button>
 
-                        <button onClick={() => window.location.href = 'https://flowcv.com/resume/u93s6l54kg'} className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800 via-purple-700 to-pink-800 hover:bg-slate-800 text-white mt-4'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
+                        <button onClick={() => window.location.href = 'https://flowcv.com/resume/u93s6l54kg'} className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-950  hover:bg-slate-800 text-white mt-4'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
