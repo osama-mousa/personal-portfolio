2b52d56a39eb41db01754f9ff8ca1e28f5a78878
osamasayd <osamasayd141@gmail.com>
2024-03-05 01:38:14 +0300

    add motion to component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,13 +2,18 @@
 import Image from 'next/image'
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation';
-
+import { motion } from 'framer-motion';
 
 const HeroSection = () => {
     return (
-        <section>
+        <section id='/'>
             <div className='grid grid-cols-1 sm:grid-cols-12'>
-                <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className='col-span-7 place-self-center text-center sm:text-left'
+                >
                     <h1 className="text-white mb-20 text-4xl sm:text-5xl lg:text-6xl font-extrabold h-10">
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-900 to-pink-900'>Hello, I&apos;m{' '}</span>
                         <TypeAnimation
@@ -33,8 +38,12 @@ const HeroSection = () => {
 
                         <button onClick={() => window.location.href = 'https://flowcv.com/resume/u93s6l54kg'} className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-950  hover:bg-slate-800 text-white mt-4'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
                     </div>
-                </div>
-                <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className='col-span-5 place-self-center mt-4 lg:mt-0'>
                     <div className='rounded-full bg-purple-950  w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative glow'>
                         <Image
                             src={'/images/me.jpeg'}
@@ -44,9 +53,9 @@ const HeroSection = () => {
                             height={300}
                         />
                     </div>
-                </div>
+                </motion.div>
             </div>
-        </section>
+        </section >
     )
 }
 
