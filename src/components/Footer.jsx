7e151e3d9a05d36250f2a95e56461eb2f990b52b
Osama Mousa <83459975+osama-mousa@users.noreply.github.com>
2024-08-13 23:00:08 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className='footer bg-[#111111] glow'>
             <div className='container p-16 flex justify-between'>
-                <span>LOGO</span>
+                <span>Osama</span>
                 <p className='text-slate-600'>All rights reserved.</p>
             </div>
         </footer>
