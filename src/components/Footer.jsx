f07b1ae1b61d6441785953c4b25087d197e270ab
osamasayd <osamasayd141@gmail.com>
2024-08-16 02:53:43 +0300

    set logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
     return (
         <footer className='footer bg-[#111111] glow'>
             <div className='container p-16 flex justify-between'>
-                <span>Oss</span>
+                <Link href={'/'}>
+                    <Image
+                        src="/images/OSM.svg"
+                        alt="Logo"
+                        width={50}
+                        height={20}
+                        className={`cursor-pointer filter brightness-0 invert`}
+                    />
+                </Link>
                 <p className='text-slate-600'>All rights reserved.</p>
             </div>
         </footer>
